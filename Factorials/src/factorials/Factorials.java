8dc822eb988f47cf48743c8782e99dc20f177525
AmiyaSmith19 <60448848+AmiyaSmith19@users.noreply.github.com>
2020-02-06 14:15:23 -0500

    Add files via upload

---
+++ Factorials/src/factorials/Factorials.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package factorials;
+/**
+ *
+ * @author amiya
+ */
+public class Factorials {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int i, Factorial = 1;
+       for(i=1; i<=10; i++)
+
+{
+
+Factorial = Factorial * i;
+
+    System.out.println("Factorial of " + i + " is: " + Factorial);
+
+}
+    }
+    }
+
+
